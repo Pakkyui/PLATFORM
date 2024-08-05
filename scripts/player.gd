12d6d08b56fc15d08800b844507ebd8c042c3f0c
Pakkyui <djanaya12@gmail.com>
2024-08-05 15:04:08 -0500

    states

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,38 +1,56 @@
 extends CharacterBody2D
  
 signal blade
-
+enum States{AIR = 1, FLOOR, CHARGE, WALL}
+var state = States.AIR
 var can_blade: bool = true
-
 @export var speed = 150
 @export var gravity = 30
-@export var jump_force = 300 
+@export var jump_force = 200 
 @export var blade_force = 400
-
+@export var air_fric = 1300
 @onready var ap = $AnimationPlayer
 @onready var sprite = $Sprite2D
 const TBlade = preload("res://scenes/Blade.tscn")
 
 #Input
 func _physics_process(_delta):
+	match state:
+		States.AIR:
+			if is_on_floor():
+				state = States.FLOOR
+			var horizontal_direction = Input.get_axis("left" , "right")
+			if horizontal_direction != 0:
+				velocity.x = move_toward(velocity.x, speed * horizontal_direction, air_fric * _delta)
+			else:
+				velocity.x = move_toward(velocity.x, 0 ,air_fric * _delta)
+			if horizontal_direction != 0:
+				sprite.flip_h = (horizontal_direction == -1)
+			move_and_fall()
+			update_animations(horizontal_direction)
+			fire()
+		States.FLOOR:
+			if not is_on_floor():
+				state = States.AIR
+			var horizontal_direction = Input.get_axis("left" , "right")
+			velocity.x = speed * horizontal_direction
+			if horizontal_direction != 0:
+				sprite.flip_h = (horizontal_direction == -1)
+			if Input.is_action_just_pressed("jump"):
+				velocity.y = -jump_force
+				state = States.AIR
+			move_and_fall()
+			update_animations(horizontal_direction)
+			fire()
+			
+		
+func move_and_fall():
 	if !is_on_floor():  
 		velocity.y += gravity
 		if velocity.y > 1000:
 			velocity.y = 1000
-	
-	if Input.is_action_just_pressed("up"): #&& is_on_floor():
-		velocity.y = -jump_force
-	
-	var horizontal_direction = Input.get_axis("left" , "right")
-	velocity.x = speed * horizontal_direction
-	if horizontal_direction != 0:
-		sprite.flip_h = (horizontal_direction == -1)
-	
-	
 	move_and_slide()
-	fire()
-	update_animations(horizontal_direction)
-
+	
 func update_animations(horizontal_direction):
 	if is_on_floor():
 		if horizontal_direction == 0:
