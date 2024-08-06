6d7504542750c364313d6ace04d5dd6de002ebb2
Pakkyui <djanaya12@gmail.com>
2024-08-05 20:52:23 -0500

    blade states

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,37 +1,39 @@
 extends CharacterBody2D
  
 signal blade
-enum States{AIR = 1, FLOOR, CHARGE, WALL}
+enum States{AIR = 1, FLOOR, CHARGE, WALL, DASH, PREDASH}
 var state = States.AIR
 var can_blade: bool = true
+var air_fric = 1300
 @export var speed = 150
 @export var gravity = 30
 @export var jump_force = 200 
 @export var blade_force = 400
-@export var air_fric = 1300
 @onready var ap = $AnimationPlayer
 @onready var sprite = $Sprite2D
 const TBlade = preload("res://scenes/Blade.tscn")
 
 #Input
 func _physics_process(_delta):
+	print(state)
+	cblade()
 	match state:
 		States.AIR:
 			if is_on_floor():
 				state = States.FLOOR
-			var horizontal_direction = Input.get_axis("left" , "right")
-			if horizontal_direction != 0:
-				velocity.x = move_toward(velocity.x, speed * horizontal_direction, air_fric * _delta)
-			else:
-				velocity.x = move_toward(velocity.x, 0 ,air_fric * _delta)
-			if horizontal_direction != 0:
-				sprite.flip_h = (horizontal_direction == -1)
+			if Input.is_action_just_pressed("blade") && can_blade:
+				state = States.PREDASH
+				$PREDASHTIME.start()
+			momentum()
 			move_and_fall()
 			update_animations(horizontal_direction)
-			fire()
 		States.FLOOR:
+			if Input.is_action_just_pressed("blade") && can_blade:
+				state = States.PREDASH
+				$PREDASHTIME.start()
 			if not is_on_floor():
 				state = States.AIR
+				can_blade = true
 			var horizontal_direction = Input.get_axis("left" , "right")
 			velocity.x = speed * horizontal_direction
 			if horizontal_direction != 0:
@@ -41,9 +43,37 @@ func _physics_process(_delta):
 				state = States.AIR
 			move_and_fall()
 			update_animations(horizontal_direction)
+		States.PREDASH:
+			if Input.is_action_just_released("blade"):
+				state = States.DASH
+			if $PREDASHTIME.is_stopped():
+				state = States.CHARGE
+			move_and_fall()
+		States.DASH:
 			fire()
-			
-		
+			if not is_on_floor():
+				state = States.AIR
+			else:
+				state = States.FLOOR
+		States.CHARGE:
+			momentum()
+			if Input.is_action_just_released("blade"):
+				fire()
+				if not is_on_floor():
+					state = States.AIR
+				else:
+					state = States.FLOOR
+			charge_and_fall()
+				
+func charge_and_fall():
+	if !is_on_floor():  
+		velocity.y = gravity
+		if velocity.y > 0.1:
+			velocity.y = 0.1
+		if velocity.x > 30:
+			velocity.x = 30
+	move_and_fall()
+
 func move_and_fall():
 	if !is_on_floor():  
 		velocity.y += gravity
@@ -65,7 +95,7 @@ func update_animations(horizontal_direction):
 
 	#Blade shooting input
 func fire():
-	if Input.is_action_just_pressed("blade") && can_blade: 
+	if can_blade: 
 		var direction = 1 if not sprite.flip_h else -1
 		var t = TBlade.instantiate()
 		t.direction = direction
@@ -73,24 +103,25 @@ func fire():
 		t.position.y = position.y if not Input.is_action_pressed("up") else position.y - 25
 		t.position.x = position.x + 20 * direction if not Input.is_action_pressed("up") else position.x 
 		can_blade = false
+
+func cblade():
+	if Input.is_action_just_released("blade") && can_blade:
 		$Timer.start()
 		blade.emit()
-		
-	if not $Timer.is_stopped():
-		can_blade = false
+	
 	if $Timer.is_stopped():
 		if is_on_floor():
 			can_blade = true
-			
-func on_timer_timeout():
-	pass
-			
-			
-
-
-
-			
-			
 		
-		
+func _on_predashtime_timeout():
+	if Input.is_action_pressed("blade"):
+		state = States.CHARGE
+func momentum():
+	var horizontal_direction = Input.get_axis("left" , "right")
+	if horizontal_direction != 0:
+		velocity.x = move_toward(velocity.x, 20 * horizontal_direction, air_fric * _delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0 ,air_fric * _delta)
+	if horizontal_direction != 0:
+		sprite.flip_h = (horizontal_direction == -1)
 
